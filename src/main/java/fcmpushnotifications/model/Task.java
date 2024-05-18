48b001b25f3064f9f8ba377ed3bb23bb8a2909ad
elyra1 <afonchenkov98@bk.ru>
2024-05-18 21:08:03 +0300

    Фиксы + добавил обработку совместных и повторяющихся задач

--- src/main/java/fcmpushnotifications/model/Task.java
+++ src/main/java/fcmpushnotifications/model/Task.java
@@ -4,7 +4,13 @@ import com.google.cloud.Timestamp;
 
 import fcmpushnotifications.firebase.TimestampDeserializer;
 
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -20,9 +26,13 @@ public class Task {
     private Timestamp endTime;
     private boolean notificationSended;
     private int remindTimeInSeconds;
+    private Long color;
+    private String repeatString;
+    private List<String> contributors;
 
-    public Task(String authorId, String id, String title, String description, String category,
-            Timestamp startTime, Timestamp endTime, boolean notificationSended, int remindTimeInSeconds) {
+    public Task(String authorId, String id, String title, String description, String category, Timestamp startTime,
+            Timestamp endTime, boolean notificationSended, int remindTimeInSeconds, Long color, String repeatString,
+            List<String> contributors) {
         this.authorId = authorId;
         this.id = id;
         this.title = title;
@@ -32,38 +42,131 @@ public class Task {
         this.endTime = endTime;
         this.notificationSended = notificationSended;
         this.remindTimeInSeconds = remindTimeInSeconds;
+        this.color = color;
+        this.repeatString = repeatString;
+        this.contributors = contributors;
     }
 
     public Task() {
-        super();
+    }
+
+    public Map<String, Object> withShiftedDatesByOneDay() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.startTime.toDate());
+        cal.add(Calendar.DATE, 1);
+        Timestamp newStartTime = Timestamp.of(cal.getTime());
+
+        cal.setTime(this.endTime.toDate());
+        cal.add(Calendar.DATE, 1);
+        Timestamp newEndTime = Timestamp.of(cal.getTime());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> data = objectMapper
+                .convertValue(this, new TypeReference<Map<String, Object>>() {
+                });
+        data.put("startTime", newStartTime);
+        data.put("endTime", newEndTime);
+        data.put("color", color);
+        data.put("notificationSended", false);
+        return data;
+    }
+
+    public Map<String, Object> withShiftedDatesByOneWeek() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.startTime.toDate());
+        cal.add(Calendar.WEEK_OF_YEAR, 1);
+        Timestamp newStartTime = Timestamp.of(cal.getTime());
+
+        cal.setTime(this.endTime.toDate());
+        cal.add(Calendar.WEEK_OF_YEAR, 1);
+        Timestamp newEndTime = Timestamp.of(cal.getTime());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> data = objectMapper
+                .convertValue(this, new TypeReference<Map<String, Object>>() {
+                });
+        data.put("startTime", newStartTime);
+        data.put("endTime", newEndTime);
+        data.put("color", color);
+        data.put("notificationSended", false);
+        return data;
+    }
+
+    public Map<String, Object> withShiftedDatesByOneMonth() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.startTime.toDate());
+        cal.add(Calendar.MONTH, 1);
+        Timestamp newStartTime = Timestamp.of(cal.getTime());
+
+        cal.setTime(this.endTime.toDate());
+        cal.add(Calendar.MONTH, 1);
+        Timestamp newEndTime = Timestamp.of(cal.getTime());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> data = objectMapper
+                .convertValue(this, new TypeReference<Map<String, Object>>() {
+                });
+        data.put("startTime", newStartTime);
+        data.put("endTime", newEndTime);
+        data.put("color", color);
+        data.put("notificationSended", false);
+        return data;
     }
 
     public String getAuthorId() {
         return authorId;
     }
 
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getCategory() {
         return category;
     }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public Timestamp getStartTime() {
         return startTime;
     }
 
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
     public Timestamp getEndTime() {
         return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
     }
 
     public boolean isNotificationSended() {
@@ -80,5 +183,29 @@ public class Task {
 
     public void setRemindTimeInSeconds(int remindTimeInSeconds) {
         this.remindTimeInSeconds = remindTimeInSeconds;
+    }
+
+    public Long getColor() {
+        return color;
+    }
+
+    public void setColor(Long color) {
+        this.color = color;
+    }
+
+    public String getRepeatString() {
+        return repeatString;
+    }
+
+    public void setRepeatString(String repeatString) {
+        this.repeatString = repeatString;
+    }
+
+    public List<String> getContributors() {
+        return contributors;
+    }
+
+    public void setContributors(List<String> contributors) {
+        this.contributors = contributors;
     }
 }
--- src/main/java/fcmpushnotifications/service/PushNotificationService.java
+++ src/main/java/fcmpushnotifications/service/PushNotificationService.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.EventListener;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreException;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 
 import fcmpushnotifications.firebase.FCMService;
 import fcmpushnotifications.model.PushNotificationRequest;
@@ -18,6 +20,7 @@ import fcmpushnotifications.model.Task;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -32,18 +35,88 @@ public class PushNotificationService {
     private Logger logger = LoggerFactory.getLogger(PushNotificationService.class);
     private FCMService fcmService;
     private Firestore firestore;
+    private List<Task> upcomingTasks = new ArrayList<>();
+    private List<String> userIds = new ArrayList<String>();
 
-    public PushNotificationService(FCMService fcmService, Firestore firestore) {
+    public PushNotificationService(FCMService fcmService, Firestore firestore) throws Exception {
         this.fcmService = fcmService;
         this.firestore = firestore;
+        firestore.collection("Tasks").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot arg0, FirestoreException arg1) {
+                System.out.print("TASKS: " + upcomingTasks.size() + ", ");
+                upcomingTasks = getTasksFromQuerySnapshot(arg0);
+                System.out.println(" CHANGED: " + upcomingTasks.size());
+            }
+        });
+    }
+
+    public List<Task> getTasksFromQuerySnapshot(QuerySnapshot arg) {
+        final ObjectMapper mapper = new ObjectMapper();
+        return arg.getDocuments().stream().map(e -> mapper.convertValue(e.getData(), Task.class))
+                .collect(Collectors.toList());
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void addRepeatTask() throws Exception {
+
+        List<Task> repeatedTasks = new ArrayList<>();
+        final ObjectMapper mapper = new ObjectMapper();
+        for (String userId : userIds) {
+            repeatedTasks.addAll(getAllTasksForUser(userId).stream().map(e -> mapper.convertValue(e, Task.class))
+                    .filter(task -> task.getRepeatString() != "never")
+                    .collect(Collectors.toList()));
+        }
+        List<Task> dayRepeat = repeatedTasks.stream().filter(task -> task.getRepeatString().equals("day"))
+                .collect(Collectors.toList());
+        List<Task> weekRepeat = repeatedTasks.stream().filter(task -> task.getRepeatString().equals("week"))
+                .collect(Collectors.toList());
+        List<Task> monthRepeat = repeatedTasks.stream().filter(task -> task.getRepeatString().equals("month"))
+                .collect(Collectors.toList());
+        Calendar cal = Calendar.getInstance();
+        Calendar nowCal = Calendar.getInstance();
+        int nowDayOfYear = nowCal.get(Calendar.DAY_OF_YEAR);
+
+        for (Task task : dayRepeat) {
+            cal.setTime(task.getStartTime().toDate());
+            int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
+            if (nowDayOfYear - dayOfYear >= 1) {
+                firestore.collection("Tasks").document(task.getId()).set(task.withShiftedDatesByOneDay());
+            }
+        }
+        for (Task task : weekRepeat) {
+            cal.setTime(task.getStartTime().toDate());
+            int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
+            if (nowDayOfYear - dayOfYear >= 1) {
+                firestore.collection("Tasks").document(task.getId()).set(task.withShiftedDatesByOneWeek());
+            }
+        }
+        for (Task task : monthRepeat) {
+            cal.setTime(task.getStartTime().toDate());
+            int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
+            if (nowDayOfYear - dayOfYear >= 1) {
+                firestore.collection("Tasks").document(task.getId()).set(task.withShiftedDatesByOneMonth());
+            }
+        }
     }
 
     @Scheduled(fixedDelay = 60000) // Запуск каждые 1 минут
-    public void checkUpcomingTasks() throws Exception {
-        List<Task> upcomingTasks = getUpcomingTasks();
+    public void checkIsReadyForNotification() throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        for (Task task : upcomingTasks) {
+        List<Task> needNotification = upcomingTasks.stream().filter(task -> {
+            long currentTimeMillis = Timestamp.now().getSeconds();
+            long startTimeMillis = task.getStartTime().getSeconds();
+            long differenceInMillis = startTimeMillis - currentTimeMillis;
+            return differenceInMillis <= task.getRemindTimeInSeconds() && !task.isNotificationSended();
+        }).collect(Collectors.toList());
+        for (Task task : needNotification) {
             String fcmToken = getFcmTokenForUser(task.getAuthorId());
+            List<String> contributorsTokens = new ArrayList<>();
+            if (task.getContributors().size() != 0) {
+                for (String contributorId : task.getContributors()) {
+                    contributorsTokens.add(getFcmTokenForUser(contributorId));
+                }
+            }
 
             if (fcmToken != null) {
                 Map<String, Object> data = firestore.collection("Tasks").document(task.getId()).get().get().getData();
@@ -54,6 +127,11 @@ public class PushNotificationService {
                         "topic");
                 req.setToken(fcmToken);
                 sendPushNotificationToToken(req);
+                for (String contributorToken : contributorsTokens) {
+                    req.setToken(contributorToken);
+                    sendPushNotificationToToken(req);
+                }
+                upcomingTasks.remove(task);
             }
         }
     }
@@ -66,14 +144,6 @@ public class PushNotificationService {
         return null;
     }
 
-    private List<String> getAllUserIds() throws Exception {
-        List<String> userIds = new ArrayList<>();
-        for (QueryDocumentSnapshot document : firestore.collection("Users").get().get().getDocuments()) {
-            userIds.add(document.getString("id"));
-        }
-        return userIds;
-    }
-
     private List<Map<String, Object>> getAllTasksForUser(String userId) throws Exception {
         List<Map<String, Object>> tasks = new ArrayList<>();
         for (QueryDocumentSnapshot document : firestore.collection("Tasks")
@@ -84,25 +154,6 @@ public class PushNotificationService {
             tasks.add(document.getData());
         }
         return tasks;
-    }
-
-    private List<Task> getUpcomingTasks() throws Exception {
-        List<Task> upcomingTasks = new ArrayList<>();
-        List<String> userIds = getAllUserIds();
-        final ObjectMapper mapper = new ObjectMapper();
-        for (String userId : userIds) {
-            List<Task> usersTask = getAllTasksForUser(userId).stream().map(e -> mapper.convertValue(e, Task.class))
-                    .collect(Collectors.toList());
-            List<Task> needNotificationTasks = usersTask.stream().filter(task -> {
-                long currentTimeMillis = Timestamp.now().getSeconds();
-                long startTimeMillis = task.getStartTime().getSeconds();
-                long differenceInMillis = startTimeMillis - currentTimeMillis;
-                return differenceInMillis <= task.getRemindTimeInSeconds() && !task.isNotificationSended();
-            })
-                    .collect(Collectors.toList());
-            upcomingTasks.addAll(needNotificationTasks);
-        }
-        return upcomingTasks;
     }
 
     public void sendPushNotificationToToken(PushNotificationRequest request) {
